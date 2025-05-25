e2900ea8a93a12baaeb2a81d66a00a915863ae40
widojansen <widojansen@gmail.com>
2025-05-25 20:32:58 +0200

    bump to 0.1.6.2

--- ragtools/__init__.py
+++ ragtools/__init__.py
@@ -4,4 +4,4 @@ RAG Tools - A toolkit for creating Retrieval-Augmented Generation applications
 
 from ragtools.streamlit_ui import RagStreamlitUI, launch_streamlit_ui
 
-__version__ = "0.1.6.1"
+__version__ = "0.1.6.2"
