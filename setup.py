492e6d82bb15619d8dfc689e3e9ad3c01099d729
widojansen <widojansen@gmail.com>
2025-05-25 22:50:28 +0200

    fix ollama run parameters and bump to 0.1.6.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ragtools",
-    version="0.1.6.6",
+    version="0.1.6.7",
     packages=find_packages(),  # This is crucial for proper package discovery
     install_requires=[
         "streamlit",
